f43da278ae7c5b8d5d28044e4b5b52ad11fd3fda
Valdemar Júnior <valdemarjuniorr@gmail.com>
2020-02-26 23:11:29 -0300

    added message after save jobs names

--- scripts/options.js
+++ scripts/options.js
@@ -1,10 +1,23 @@
+$(document).ready(async function () {
+  let sandboxJob = await getFromStorage('sandbox');
+  $("#sandbox").val(sandboxJob);
+  let productionJob = await getFromStorage('production');
+  $("#production").val(productionJob);
+});
+
 $("#buttonId").click(async function () {
   let sandboxJob = $('#sandbox').val();
   let productionJob = $('#production').val();
   saveStorage('sandbox', sandboxJob);
   saveStorage('production', productionJob);
+  $('#saved').show();
+
 });
 
 function saveStorage(key, value) {
   chrome.storage.sync.set({[key]: value});
+}
+
+async function getFromStorage(key) {
+  return new Promise(res => chrome.storage.sync.get([key], result => res(result[key])));
 }
